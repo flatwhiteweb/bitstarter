a461e94d676e944184d6f9582e1c663c3fd85f95
Andre Kusuma <andre@flatwhiteweb.com.au>
2013-07-09 14:33:50 +0000

    read buffer3rd

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
    response.send(
-   var buf = new Buffer(fs.readFileSync("index.html","utf8"));
+   buf = new Buffer(fs.readFileSync("index.html","utf8"));
     buf.toString();
     );
  });
